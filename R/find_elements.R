834a042a313d703d9154f46cce3fdd9298518d57
jbien <jacob.bien@gmail.com>
2017-03-19 00:54:02 -0400

    Add functionality for finding the path to each element's first occurrence from root.

---
+++ R/find_elements.R
@@ -0,0 +1,36 @@
+#' Find the path from root to highest occurrence of each element
+#' 
+#' A protoclust object has a prototype associated with each interior node.
+#' Every element being clustered occurs at least as a leaf but might also 
+#' appear multiple times as a prototype.  This function finds for each element
+#' the path from the root to the highest occurrence of that element.  The path
+#' is specified by a series of 0s and 1s, where 0 means "go left" and 1 means
+#' "go right".
+#' 
+#' @param hc a protoclust object
+#' 
+#' @return a list of length n giving for each element the path from
+#' root to its highest occurrence. A 0 means go left, a 1 means go right.
+#' @export
+find_elements <- function(hc) {
+  n <- length(hc$order)
+  paths <- vector("list", n) 
+  # paths[[i]] will have path from root to element i
+  int_paths <- vector("list", n - 1) # int_paths[[n-1]] is root
+  # int_paths[[i]] will have path from root to interior node i
+  for (i in seq(n - 1, 1)) {
+    ii <- hc$merge[i, ] # i-th row of merge
+    for (j in 1:2) {
+      new_path <- c(int_paths[[i]], j - 1)
+      if (ii[j] < 0) {
+        if (is.null(paths[[-ii[j]]]))
+          paths[[-ii[j]]] <- new_path
+      } else {
+        int_paths[[ii[j]]] <- new_path
+        if (is.null(paths[[hc$protos[ii[j]]]]))
+          paths[[hc$protos[ii[j]]]] <- new_path
+      }
+    }
+  }
+  paths
+}
